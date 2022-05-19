2c7182a901a193f26ccd11b3742e2755aab2866d
Eldes <github@eldes.com>
2022-05-19 19:13:01 -0300

    Adicionado formulario de Novo Item

--- src/pages/Itens.tsx
+++ src/pages/Itens.tsx
@@ -1,20 +1,48 @@
+import { useState } from 'react';
 import Botao from '../components/Botao';
 import Cabecalho from '../components/Cabecalho';
 import Rodape from '../components/Rodape';
 import './Itens.css';
 
 const ItensPage = function() {
+    //let estado = 'INICIAL';
+    const [estado, setEstado] = useState('INICIAL');
+
+    const botaoCriarClicado = function() {
+        //estado = 'CRIAR';
+        setEstado('CRIAR');
+    };
+
     return (
         <>
             <Cabecalho />
             <main>
+                <button onClick={botaoCriarClicado}>Teste</button>
                 <h1>Itens</h1>
                 <ul>
                     <li>Item 1</li>
                     <li>Item 2</li>
                     <li>Item 3</li>
                 </ul>
-                <Botao texto="Criar" />
+                {(estado === 'INICIAL') && (
+                    <Botao texto="Criar" />
+                )}
+                {(estado === 'CRIAR') && (
+                    <form className="item">
+                    <label>
+                        Nome:
+                        <input type="text" placeholder="Nome" />
+                    </label>
+                    <label>
+                        Descrição:
+                        <textarea placeholder="Descrição"></textarea>
+                    </label>
+                    <div>
+                        <Botao texto="Salvar" />
+                        <Botao texto="Cancelar" />
+                    </div>
+                    </form>
+                )} 
             </main>
             <Rodape />
             
